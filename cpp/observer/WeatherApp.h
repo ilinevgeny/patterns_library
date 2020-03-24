eb2693b98f7a39a1fabc3394620169dfa6a8e982
Evgeny Ilin <xzenya@gmail.com>
2020-03-24 17:37:45 +0300

    start learning design patterns

---
+++ cpp/observer/WeatherApp.h
@@ -0,0 +1,8 @@
+//
+// Created by ilin on 24.03.2020.
+//
+
+#ifndef CPP_PATTERNS_WEATHERAPP_H
+#define CPP_PATTERNS_WEATHERAPP_H
+
+#endif //CPP_PATTERNS_WEATHERAPP_H
---
+++ cpp/strategy/Duck.h
@@ -0,0 +1,14 @@
+//
+// Created by ilin on 24.03.2020.
+//
+
+#ifndef CPP_PATTERNS_DUCK_H
+#define CPP_PATTERNS_DUCK_H
+
+
+class Duck {
+
+};
+
+
+#endif //CPP_PATTERNS_DUCK_H
